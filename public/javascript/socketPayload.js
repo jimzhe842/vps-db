b55ffff84c1c834a4a3f61461f85f6c9429d25b3
Jimmy Zheng <jimmy_842@berkeley.edu>
2021-06-06 00:28:43 -0700

    updated the client javascript socket

--- public/javascript/socketPayload.js
+++ public/javascript/socketPayload.js
@@ -9,12 +9,18 @@ document.addEventListener('DOMContentLoaded', () => {
     if (uuidElement.textContent !== uuid) { return }
     const payloadsList = document.querySelector("#payloads-list");
     let wrapper = document.createElement("li");
+    wrapper.className = "outer";
+    let payloadTitle = document.createElement("h5");
+    h5.textContent = "Payload";
+    h5.className = "payload-title";
     let ul = document.createElement("ul");
     for (prop of Object.keys(payload)) {
       let pair = document.createElement("li");
+      pair.className = "inner"
       pair.innerHTML = `<h5>${prop}</h5><p>${payload[prop]}</p>`;
       ul.appendChild(pair);
     }
+    wrapper.appendChild(payloadTitle);
     wrapper.appendChild(ul);
     payloadsList.appendChild(wrapper);
     console.log(true);
